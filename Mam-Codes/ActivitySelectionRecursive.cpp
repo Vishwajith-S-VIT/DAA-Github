3d32321acf82a20dc4967a20595b9c85b9263a80
Vishwajith-S-VIT <vishwajith.s2023@vitstudent.ac.in>
2025-04-09 22:46:45 +0530

    Added PAT-1 Mam's codes.

--- Mam-Codes/ActivitySelectionRecursive.cpp
+++ Mam-Codes/ActivitySelectionRecursive.cpp
@@ -2,48 +2,33 @@
 using namespace std;
 #include<queue>
 #include<set>
-/*11
-a3 0 6
-a1 1 4
-a4 5 7
-a2 3 5
-a5 3 9
-a11 12 16
-a6 5 9
-a9 8 12
-a7 6 10
-a8 8 11
-a10 2 14
-*/
-class activity
-{
+
+class activity{
     public:
-    string name;
-    int start;
-    int finish;
-    friend istream& operator>>(istream&,activity&);
-    friend ostream& operator<<(ostream&,const activity&);
-    bool operator<(const activity&) const;
+        string name;
+        int start;
+        int finish;
+        friend istream& operator >>(istream&,activity&);
+        friend ostream& operator <<(ostream&,const activity&);
+        bool operator <(const activity&) const;
 };
-istream& operator>>(istream& in, activity& a)
-{
+
+istream& operator >>(istream& in, activity& a){
     in>>a.name>>a.start>>a.finish;
     return in;
 }
-ostream& operator<<(ostream& out, const activity& a)
-{
+
+ostream& operator <<(ostream& out, const activity& a){
     out<<a.name<<" ";
     return out;
 }
 // overload less than operator so that priority queue can be 
 // constructed as desired. We intend to create a min priority
 // queue based on finish time of activity
-bool activity::operator<(const activity& a) const
-{
+bool activity::operator <(const activity& a) const{
     return a.finish<finish;
 }
-void recursive_activity_selector(priority_queue<activity> &pq,vector<activity> &v)
-{
+void recursive_activity_selector(priority_queue<activity> &pq,vector<activity> &v){
     activity a;
     // take activity with minimum finish time
     a = pq.top();
@@ -53,13 +38,11 @@ void recursive_activity_selector(priority_queue<activity> &pq,vector<activity> &
     // while there are activities in the priority queue
     // and start time of top activity in pq is less than
     // finish time of last chosen activity
-    while(!pq.empty()&&pq.top().start<a.finish)
-    {
+    while(!pq.empty()&&pq.top().start<a.finish){
         // remove top activity from priority queue
         pq.pop();
     }
-    if(!pq.empty())
-    {
+    if(!pq.empty()){
         // if priority queue is not empty then
         // make recursive call to the function
         recursive_activity_selector(pq,v);
@@ -77,8 +60,7 @@ int main()
     cin>>n;
     // read input activities and create a priority queue based on
     // finish time 
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         cin>>a;
         pq.push(a);
     }
